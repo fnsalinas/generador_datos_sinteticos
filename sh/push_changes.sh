9616ad19fd6bb526d027de3faecf774db17b1ae5
Fabio Salinas <fabio.salinas1982@gmail.com>
2023-02-09 12:19:09 -0500

    Agregados cambios al push_changes para que pida mensaje de commit 2023-02-09 12:18:50

--- sh/push_changes.sh
+++ sh/push_changes.sh
@@ -3,9 +3,20 @@
 # Guarda la fecha/hora en una variable
 fecha=$(date +"%Y-%m-%d %H:%M:%S")
 
+# Ask for the Commit message and save it in a variable
+echo "Introduce el mensaje del commit y pulsa [ENTER]:"
+read mensaje
+
+# Si no se introduce mensaje, se pone uno por defecto
+if [ -z "$mensaje" ]
+then
+    mensaje="Commit automatico"
+fi
+
+
 cd ~/workspace/fsalinas/generador_datos_sinteticos
 
 git status;
 git add .;
-git commit -m "Actualización de datos $fecha";
+git commit -m "$mensaje $fecha";
 git push;
