a12692fcb5430adf0791cdec534f45dd1094732e
Fabio Salinas <fabio.salinas1982@gmail.com>
2023-02-10 12:13:45 -0500

    Arreglos archivo startup 2023-02-10 12:13:35 COT

--- sh/startup.sh
+++ sh/startup.sh
@@ -5,10 +5,10 @@ if ! command -v python3.10 &> /dev/null
 then
     echo "Python 3.10 could not be found"
     echo "Installing Python 3.10"
-    sudo apt update && sudo apt upgrade -y;
-    sudo apt install software-properties-common -y;
-    sudo add-apt-repository ppa:deadsnakes/ppa;
-    sudo apt install python3.10 -y;
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt install python3.10 -y
 else
     echo "Python 3.10 already installed"
 fi
@@ -18,11 +18,11 @@ if ! command -v pip &> /dev/null
 then
     echo "pip could not be found"
     echo "Installing pip"
-    sudo apt update && sudo apt upgrade -y;
-    sudo apt install python3-pip -y;
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install python3-pip -y
 
-    sudo apt-get update -y;
-    sudo apt install python3-pip -y;
+    sudo apt-get update -y
+    sudo apt install python3-pip -y
 else
     echo "pip already installed"
 fi
@@ -32,9 +32,9 @@ if ! command -v pipenv &> /dev/null
 then
     echo "pipenv could not be found"
     echo "Installing pipenv"
-    sudo apt update && sudo apt upgrade -y;
-    sudo apt install python3-pip -y;
-    pip install pipenv;
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install python3-pip -y
+    pip install pipenv
 else
     echo "pipenv already installed"
 fi
@@ -44,8 +44,8 @@ if ! command -v git &> /dev/null
 then
     echo "git could not be found"
     echo "Installing git"
-    sudo apt update && sudo apt upgrade -y;
-    sudo apt install git -y;
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install git -y
 else
     echo "git already installed"
 fi
@@ -58,22 +58,22 @@ if [ -d "~/generador_datos_sinteticos" ]; then
 else
     echo "~/generador_datos_sinteticos does not exist"
     git clone
-    cd ~/generador_datos_sinteticos/;
-
+    cd ~/generador_datos_sinteticos/
+fi
 
 if grep -q "CUSTOM SCRIPTS" ~/.bashrc; then
     echo "CUSTOM SCRIPTS already exists in ~/.bashrc"
 else
     echo "CUSTOM SCRIPTS does not exist in ~/.bashrc"
-    echo "" >> ~/.bashrc;
-    echo "" >> ~/.bashrc;
-    echo "" >> ~/.bashrc;
-    echo "# ----------- CUSTOM SCRIPTS -----------" >> ~/.bashrc;
-    echo "" >> ~/.bashrc;
-    echo "" >> ~/.bashrc;
-    echo "" >> ~/.bashrc;
-    echo "alias python='python3'" >> ~/.bashrc;
+    echo "" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "# ----------- CUSTOM SCRIPTS -----------" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "alias python='python3'" >> ~/.bashrc
 fi
 
-source ~/.bashrc;
-cd ~/generador_datos_sinteticos/;
+source ~/.bashrc
+cd ~/generador_datos_sinteticos/
