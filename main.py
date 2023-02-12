2fe62e2719c86d4d367fc89463cad677925658a2
Fabio Salinas <fabio.salinas1982@gmail.com>
2023-02-12 18:56:38 -0500

    Actualizaciones 2023-02-12 18:56:27 COT

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from typing import List, Tuple, Dict, Any
 from fastapi import FastAPI
 import json
 
-from data_generator import FULL_DATA
+from data_generator import get_full_data
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    FULL_DATA: Dict[str, Any] = get_full_data()
     return json.dumps(FULL_DATA, indent=4)
--- src/data_generator.py
+++ src/data_generator.py
@@ -11,21 +11,22 @@ from datagen import (
     get_invoice_data
 )
 
-# Test module functionality by printing a random user data
-client_data: Dict[str, Any] = get_client_data()
-user_data: Dict[str, Any] = get_user_data(client_data)
-product_data: Dict[str, Any] = get_product_data()
-category_data: Dict[str, Any] = get_category_data(product_data)
-order_data: Dict[str, Any] = get_orders_data(client_data, product_data)
-invoice_data: Dict[str, Any] = get_invoice_data(client_data, order_data)
 
-FULL_DATA: Dict[str, Any] = {
-    "client_data": client_data,
-    "user_data": user_data,
-    "product_data": product_data,
-    "category_data": category_data,
-    "order_data": order_data,
-    "invoice_data": invoice_data
-}
+def get_full_data() -> Dict[str, Any]:
+    client_data: Dict[str, Any] = get_client_data()
+    user_data: Dict[str, Any] = get_user_data(client_data)
+    product_data: Dict[str, Any] = get_product_data()
+    category_data: Dict[str, Any] = get_category_data(product_data)
+    order_data: Dict[str, Any] = get_orders_data(client_data, product_data)
+    invoice_data: Dict[str, Any] = get_invoice_data(client_data, order_data)
 
-# print(json.dumps(FULL_DATA, indent=4))
+    FULL_DATA: Dict[str, Any] = {
+        "client_data": client_data,
+        "user_data": user_data,
+        "product_data": product_data,
+        "category_data": category_data,
+        "order_data": order_data,
+        "invoice_data": invoice_data
+    }
+    
+    return FULL_DATA
