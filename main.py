9cce690a953d64694151f9e31d3a4c6599f92243
Fabio Nelson Salinas Moreno <39883976+fnsalinas@users.noreply.github.com>
2023-02-12 15:43:59 -0500

    Add test to Path
    Probando la librería pathlib y la función Path para identificar la ruta de la instalación.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+
 from typing import List, Tuple, Dict, Any
 from fastapi import FastAPI
 import json
